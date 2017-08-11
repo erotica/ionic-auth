5167c1aed86a5b1d34a2a47455414c66434f79c8
unknown <eroica7@gmail.com>
2017-08-11 18:05:19 +0900

    todo 보완

--- src/pages/todo/todo.ts
+++ src/pages/todo/todo.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { NgClass } from '@angular/common';
+
+import { LoadingController, ToastController } from 'ionic-angular';
+import { AuthService } from '../../providers/auth-service';
 
 /**
  * Generated class for the TodoPage page.
@@ -12,14 +16,48 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 @Component({
   selector: 'page-todo',
   templateUrl: 'todo.html',
+  
 })
-export class TodoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+export class TodoPage {
+  
+  loading: any;
+  stocks:Array<{id: string, title: string}>=[];
+
+  constructor(public navCtrl: NavController, public authService: AuthService,public navParams: NavParams,public loadingCtrl: LoadingController, private toastCtrl: ToastController) {}
+
+  todoSearch() {
+    this.showLoader();
+    this.authService.todoSearch(this.stocks).then((result) => {
+      this.stocks.dismiss();
+      this.stocks = result;
+    
+      //this.navCtrl.setRoot(TabsPage);
+       console.log(result);
+      
+    }, (err) => {
+      this.loading.dismiss();
+      this.presentToast(err);
+    });
+  }
+  
+  showLoader() {
+    console.log('ionViewDidLoad TodoPage');
   }
 
-  ionViewDidLoad() {
-    console.log('ionViewDidLoad TodoPage');
+  presentToast(msg) {
+    let toast = this.toastCtrl.create({
+      message: msg,
+      duration: 3000,
+      position: 'bottom',
+      dismissOnPageChange: true
+    });
+
+    toast.onDidDismiss(() => {
+      console.log('Dismissed toast');
+    });
+
+    toast.present();
   }
 
 }
--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -55,4 +55,18 @@ export class AuthService {
     });
   }
 
+   todoSearch(credentials) {
+    return new Promise((resolve, reject) => {
+        let headers = new Headers();
+        headers.append('Content-Type', 'application/json');
+        //Performs a request with post http method.
+        //this.http.post(apiUrl+'/posts', JSON.stringify(credentials), {headers: headers})
+        this.http.get(apiUrl+'/posts', {headers: headers})
+          .subscribe(res => {
+            resolve(res.json());
+          }, (err) => {
+            reject(err);
+          });
+    });
+  }
 }
