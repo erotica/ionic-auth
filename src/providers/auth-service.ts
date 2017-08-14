d7a60526f787759186360e8f890be1cb5bb910c3
unknown <eroica7@gmail.com>
2017-08-14 09:30:00 +0900

    data service에서  todosearch, onload  삭제

--- src/providers/auth-service.ts
+++ src/providers/auth-service.ts
@@ -70,38 +70,4 @@ export class AuthService {
     });
   }
  
-  load() {
-   
-  if (this.data) {
-    // already loaded data
-    return Promise.resolve(this.data);
-  }
-
-  // don't have the data yet
-  return new Promise(resolve => {
-    // We're using Angular HTTP provider to request the data,
-    // then on the response, it'll map the JSON data to a parsed JS object.
-    // Next, we process the data and resolve the promise with the new data.
-    this.http.get('path/to/data.json')
-      .map(res => res.json())
-      .subscribe(data => {
-        // we've got back the raw data, now generate the core schedule data
-        // and save the data for later reference
-        this.data = data;
-        resolve(this.data);
-      });
-  });
-}
-
-getHeroes(): Promise<Hero[]> {
-  return this.http.get(apiUrl)
-             .toPromise()
-             .then(response => response.json().data as Hero[])
-             .catch(this.handleError);
-}
- 
-private handleError(error: any): Promise<any> {
-  console.error('An error occurred', error); // for demo purposes only
-  return Promise.reject(error.message || error);
-}
 }
