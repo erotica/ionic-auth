797ab1ee59fab6b2f2021681a94c6e94c2229f93
unknown <eroica7@gmail.com>
2017-08-28 19:19:18 +0900

    Select Image 기능 추가

--- src/pages/myasset/myasset.ts
+++ src/pages/myasset/myasset.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-import { LoadingController, ToastController } from 'ionic-angular';
+import { LoadingController, ToastController ,ActionSheetController,Platform } from 'ionic-angular';
 import { BarcodeScanner, BarcodeScannerOptions} from '@ionic-native/barcode-scanner';
 import { Camera, CameraOptions } from '@ionic-native/camera';
 
 //파일전송
-import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
 import { File} from '@ionic-native/file';
+import { FileTransfer, FileUploadOptions, FileTransferObject } from '@ionic-native/file-transfer';
+import { FilePath } from '@ionic-native/file-path';
 
  
 declare var cordova: any;
@@ -44,9 +45,91 @@ export class MyassetPage {
                private camera: Camera,
                private transfer: FileTransfer,
                private file : File,
+               private filePath: FilePath,
                public loadingCtrl: LoadingController,
-               private toastCtrl: ToastController) {}
+               private toastCtrl: ToastController,
+               public actionSheetCtrl: ActionSheetController,
+               public platform: Platform) {}
+  
+ public presentActionSheet() {
+  let actionSheet = this.actionSheetCtrl.create({
+    title: 'Select Image Source',
+    buttons: [
+      {
+        text: 'Load from Library',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.PHOTOLIBRARY);
+        }
+      },
+      {
+        text: 'Use Camera',
+        handler: () => {
+          this.takePicture(this.camera.PictureSourceType.CAMERA);
+        }
+      },
+      {
+        text: 'Cancel',
+        role: 'cancel'
+      }
+    ]
+  });
+  actionSheet.present();
+}
 
+public takePicture(sourceType) {
+  // Create options for the Camera Dialog
+  var options = {
+    quality: 100,
+    sourceType: sourceType,
+    saveToPhotoAlbum: false,
+    correctOrientation: true
+  };
+ 
+  // Get the data of an image
+  this.camera.getPicture(options).then((imagePath) => {
+    // Special handling for Android library
+    if (this.platform.is('android') && sourceType === this.camera.PictureSourceType.PHOTOLIBRARY) {
+      this.filePath.resolveNativePath(imagePath)
+        .then(filePath => {
+          let correctPath = filePath.substr(0, filePath.lastIndexOf('/') + 1);
+          let currentName = imagePath.substring(imagePath.lastIndexOf('/') + 1, imagePath.lastIndexOf('?'));
+          this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+        });
+    } else {
+      var currentName = imagePath.substr(imagePath.lastIndexOf('/') + 1);
+      var correctPath = imagePath.substr(0, imagePath.lastIndexOf('/') + 1);
+      this.copyFileToLocalDir(correctPath, currentName, this.createFileName());
+    }
+  }, (err) => {
+    this.presentToast('Error while selecting image.');
+  });
+}
+
+// Create a new name for the image
+private createFileName() {
+  var d = new Date(),
+  n = d.getTime(),
+  newFileName =  n + ".jpg";
+  return newFileName;
+}
+ 
+// Copy the image to a local folder
+private copyFileToLocalDir(namePath, currentName, newFileName) {
+  this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
+    this.lastImage = newFileName;
+  }, error => {
+    this.presentToast('Error while storing file.');
+  });
+}
+  
+// Always get the accurate path to your apps folder
+public pathForImage(img) {
+  if (img === null) {
+    return '';
+  } else {
+    return cordova.file.dataDirectory + img;
+  }
+}
   uploadImage() {
   let url = "http://mas.hist.co.kr/upload.java";
    // File for Upload
@@ -93,33 +176,7 @@ export class MyassetPage {
 
   toast.present();
 }
-
-// Create a new name for the image
-private createFileName() {
-  var d = new Date(),
-  n = d.getTime(),
-  newFileName =  n + ".jpg";
-  return newFileName;
-}
- 
-// Copy the image to a local folder
-private copyFileToLocalDir(namePath, currentName, newFileName) {
-  this.file.copyFile(namePath, currentName, cordova.file.dataDirectory, newFileName).then(success => {
-    this.lastImage = newFileName;
-  }, error => {
-    this.presentToast('Error while storing file.');
-  });
-}
-  
-// Always get the accurate path to your apps folder
-public pathForImage(img) {
-  if (img === null) {
-    return '';
-  } else {
-    return cordova.file.dataDirectory + img;
-  }
-}
-                 
+              
   async scanBarcode() {
 
     this.options = {
@@ -136,11 +193,12 @@ public pathForImage(img) {
     console.log('ionViewDidLoad MyassetPage');
   }
    
-  async takePicture() {
+  async takePicture2() {
   this.camera.getPicture(this.cameraOptions).then((imageData) => {
     // imageData is either a base64 encoded string or a file URI
     // If it's base64:
     let captureDataUrl = 'data:image/jpeg;base64,' + imageData;
+    this.presentToast(captureDataUrl);
     console.log (captureDataUrl);
    }, (err) => {
     // Handle error
