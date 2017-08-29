d369f49a70f7056e851de4c2244da47cd0f83c1c
unknown <eroica7@gmail.com>
2017-08-29 14:58:15 +0900

    login scss 변경

--- src/pages/assetinfo/assetinfo.ts
+++ src/pages/assetinfo/assetinfo.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { SiteinspectPage } from '../siteinspect/siteinspect';
 
 /**
  * Generated class for the AssetinfoPage page.
